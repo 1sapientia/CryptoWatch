99032c717b5235914b3280e0c7129115018f8680
kgantar <klemen@rialtotrade.com>
2019-11-18 16:31:29 +0100

    initial branch out

---
+++ orderbooks/arbitrageBacktester.go
@@ -0,0 +1,7 @@
+package orderbooks
+
+// deliverables:
+// oportunity evaluation measure
+//     per pair opportunity evaluation (considering parameters like fees, oportunity sliding window, oportunity utilization strategy)
+// aggregated data for charts
+// per pair orderbooks movements (how do we visualize available volume)
